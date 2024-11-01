6f3d0fab2bb9f0cd44049e85d60d0a4e56db10ec
beethargy <alex.hojkolov@gmail.com>
2024-11-01 22:38:38 +0100

    added circle drawer and player

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
-    pygame.init
+    pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player_triangle = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
           for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                       return
-          pygame,screen.fill("black")
+                
+          screen.fill("black")
+          player_triangle.draw(screen)
           pygame.display.flip()
+          dt = clock.tick(60)/1000 #limit the framerate to 60fps
 
 
     
