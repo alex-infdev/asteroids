c99d37fa9876d6a7f8564145a1738d919ad2e398
beethargy <alex.hojkolov@gmail.com>
2024-11-03 21:41:18 +0100

    added shooting

--- asteroidfield.py
+++ asteroidfield.py
@@ -12,7 +12,9 @@ class AsteroidField(pygame.sprite.Sprite):
         ],
         [
             pygame.Vector2(-1, 0),
-            lambda y: pygame.Vector2(SCREEN_WIDTH + ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT),
+            lambda y: pygame.Vector2(
+                SCREEN_WIDTH + ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT
+            ),
         ],
         [
             pygame.Vector2(0, 1),
@@ -20,7 +22,9 @@ class AsteroidField(pygame.sprite.Sprite):
         ],
         [
             pygame.Vector2(0, -1),
-            lambda x: pygame.Vector2(x * SCREEN_WIDTH, SCREEN_HEIGHT + ASTEROID_MAX_RADIUS),
+            lambda x: pygame.Vector2(
+                x * SCREEN_WIDTH, SCREEN_HEIGHT + ASTEROID_MAX_RADIUS
+            ),
         ],
     ]
 
@@ -37,6 +41,7 @@ class AsteroidField(pygame.sprite.Sprite):
         if self.spawn_timer > ASTEROID_SPAWN_RATE:
             self.spawn_timer = 0
 
+            # spawn a new asteroid at a random edge
             edge = random.choice(self.edges)
             speed = random.randint(40, 100)
             velocity = edge[0] * speed
